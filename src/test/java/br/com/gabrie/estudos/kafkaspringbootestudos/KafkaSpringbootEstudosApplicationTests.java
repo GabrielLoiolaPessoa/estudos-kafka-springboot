9ffb6fdc831b37f97b1db65dc6a2a7694d99c114
Gabriel Pessoa <gabrieldeloiolacontato@gmail.com>
2023-01-08 20:47:07 -0300

    Adding new Kafka Configuration files

---
+++ src/test/java/br/com/gabrie/estudos/kafkaspringbootestudos/KafkaSpringbootEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrie.estudos.kafkaspringbootestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringbootEstudosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
